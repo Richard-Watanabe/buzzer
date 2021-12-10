9db0372c69717e60f401d16f56069a590232502e
Richard Watanabe <shoryuwatanabe124@gmail.com>
2021-12-10 22:20:42 +0000

    Created handleClick method

--- buzzer-client/pages/app.jsx
+++ buzzer-client/pages/app.jsx
@@ -4,14 +4,21 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      isClicked: false
     };
+    this.handleClick = this.handleClick.bind(this);
   }
 
+  handleClick(event) {
+    this.setState({
+      isOpen: false
+    });
+  }
 
   render() {
     return (
     <div className="container">
-      <button>Call</button>
+      <button onClick={this.handleClick}>Call</button>
     </div>
     );
   }
