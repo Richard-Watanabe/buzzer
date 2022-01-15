db660fcc36b612a6b033b045978b596fa34ff0ea
Jonathan Lim <jonathanlim035@gmail.com>
2022-01-14 23:46:03 -0800

    Implemented post route for new tables.

--- buzzer-server/controllers.js
+++ buzzer-server/controllers.js
@@ -8,14 +8,14 @@ const controllers = {
     get: (req, res) => {
       // Get all tables for user's business
       if (!req.params.id) {
-        res.status(404).send('No ID provided');
+        res.status(404).send('No ID Found');
       }
       // req.params.id will be business' (user) id
       businessRef.doc(req.params.id) // Points to specific document in business collection
         .get() // Gets a snapshot of the data from that document
         .then((result) => { // Does something with that data
           if (!result.exists) {
-            res.status(404).send('No users found with that ID');
+            res.status(404).send('No users found');
           }
 
           result.ref.collection('tables') // Points to the subcollection "tables" of that document
@@ -32,7 +32,30 @@ const controllers = {
 
     },
     post: (req, res) => {
-      // Post new table(s) for user's business
+      // Post new tables for user's business (Should be used on signup)
+      // All users should need to provide is the numbers for the tables in req.body
+      // Ex. ['1', '2', '3'] or ['1a', '2a', '3a']
+      if (!req.params.id) {
+        res.status(404).send('No ID Found');
+      }
+
+      businessRef.doc(req.params.id)
+        .get()
+        .then((result) => {
+          if (!result.exists) {
+            res.status(404).send('No users found');
+          }
+          const tablesRef = result.ref.collection('tables');
+
+          req.body.tables.forEach((tableNum) => {
+            tablesRef.add({
+              needsAttention: false,
+              occupied: false,
+              number: tableNum
+            }) // Probably better way to do this
+          })
+            res.status(200).send('Tables added');
+        })
     },
     put: (req, res) => {
       // Update tables for user's business
